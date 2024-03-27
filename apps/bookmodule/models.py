fe8af4d9c072fd08a39b450a97e1198edbcaf5ad
Ghadaa0 <GhadaAbdullah.cs@gmail.com>
2024-03-27 20:41:08 +0300

    commit..7

--- apps/bookmodule/models.py
+++ apps/bookmodule/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
-class Book(models.Model):
-    title = models.CharField()
-    author = models.CharField()
-    edition = models.SmallIntegerField()
     
 class Publish(models.Model):
     address = models.CharField()
  
+class Book(models.Model):
+ title = models.CharField()
+ author = models.CharField()
+ edition = models.SmallIntegerField()
